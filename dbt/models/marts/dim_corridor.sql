15bf9f1f6022712e6ce6fa6880578aafcf103e4b
Gblack98 <diopgabar1998@gmail.com>
2026-07-31 00:31:13 +0000

    Corrige l'échappement de l'apostrophe dans dim_corridor
    BigQuery rejette l'apostrophe redoublée du SQL standard : la
    littérale "Cote d'Ivoire" passe par des guillemets doubles. Le modèle
    échouait à la compilation, ce qui faisait aussi sauter le test
    d'intégrité référentielle sur pays_cible.

--- dbt/models/marts/dim_corridor.sql
+++ dbt/models/marts/dim_corridor.sql
@@ -1,5 +1,7 @@
 select 'Senegal' as pays, 'UEMOA' as zone
-union all select 'Cote d''Ivoire', 'UEMOA'
+-- Guillemets doubles : BigQuery n'accepte pas l'échappement par
+-- apostrophe redoublée du SQL standard.
+union all select "Cote d'Ivoire", 'UEMOA'
 union all select 'Mali', 'UEMOA'
 union all select 'Burkina Faso', 'UEMOA'
 union all select 'Benin', 'UEMOA'
